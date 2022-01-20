9a45b1d449349894dfe6db570be02538617b1ae1
Nanoparty <nate@Nathans-MacBook-Pro.local>
2022-01-19 23:38:43 -0500

    roll endpoint

---
+++ heros.js
@@ -0,0 +1,5 @@
+heros = ["Superman", "Alphinaud", "PieDiePie", "Jade", "Obama"];
+
+export function getHeros() {
+  return heros;
+}
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var app = express();
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://127.0.0.1:27017";
 
+heros = ["Superman", "Alphinaud", "PieDiePie", "Jade", "Obama"];
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
@@ -96,8 +98,66 @@ app.post("/register", (req, res, next) => {
             accountLevel: 1,
             accountExp: 0,
             accountExpCap: 5,
+            gems: 50,
+            heros: [],
           });
           res.json("Registration Successful");
+        }
+      });
+    }
+  );
+});
+
+app.get("/roll", (req, res, next) => {
+  console.log(JSON.stringify(req.headers));
+  var username = req.headers.username;
+
+  //check sufficient gems
+
+  //decrement gems
+
+  //roll random character
+
+  //add character to user list
+
+  //return character
+
+  MongoClient.connect(
+    url,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      if (err) {
+        return console.log(err);
+      }
+      const db = client.db("admin");
+      console.log(`MongoDB Connected: ${url}`);
+
+      const users = db.collection("users");
+
+      users.findOne({ username: username }, (err, result) => {
+        console.log("Result:", result);
+        if (result) {
+          if (result.gems >= 5) {
+            var hero = heros[Math.floor(Math.random() * heros.length)];
+            result.gems = result.gems - 5;
+            result.heros.push(hero);
+            users.updateOne(
+              { username: username },
+              {
+                $set: { gems: result.gems, heros: result.heros },
+                $currentDate: { lastModified: true },
+              }
+            );
+            var newUser = users.findOne();
+            res.json({
+              hero: hero,
+              userData: result,
+            });
+          } else {
+            res.json("Not enough gems");
+          }
+        } else {
+          res.json("Invalid User");
         }
       });
     }
